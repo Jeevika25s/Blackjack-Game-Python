4de21130ce4437ef2d0826ed2faff1b934197a53
jeevika25s <jeevika25s@gmail.com>
2026-07-02 15:33:14 +0530

    Added hit and stand functions

--- functions.py
+++ functions.py
@@ -12,3 +12,31 @@ def take_bet(chips):
 
         except ValueError:
             print("Please enter a valid number.")
+            
+# functions.py
+
+playing = True
+
+def hit(deck, hand):
+    card = deck.deal_one()
+    hand.add_card(card)
+    hand.adjust_for_ace()
+
+
+def hit_or_stand(deck, hand):
+    global playing
+
+    while True:
+        choice = input("Hit or Stand? Enter 'h' or 's': ").lower()
+
+        if choice == 'h':
+            hit(deck, hand)
+            break
+
+        elif choice == 's':
+            print("Player stands. Dealer's turn.")
+            playing = False
+            break
+
+        else:
+            print("Invalid input. Please enter h or s.")
--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from chips import Chips
-from functions import take_bet
+from deck import Deck
+from hand import Hand
+from functions import hit
 
-player = Chips()
+deck = Deck()
+deck.shuffle()
 
-print("Total Chips:", player.total)
+player_hand = Hand()
 
-take_bet(player)
+# Give two cards
+player_hand.add_card(deck.deal_one())
+player_hand.add_card(deck.deal_one())
 
-print("Bet Placed:", player.bet)
+print("Player Cards:")
+for card in player_hand.cards:
+    print(card)
+
+print("Value:", player_hand.value)
+
+print("\nGiving one more card...\n")
+
+hit(deck, player_hand)
+
+print("Updated Cards:")
+for card in player_hand.cards:
+    print(card)
+
+print("New Value:", player_hand.value)
